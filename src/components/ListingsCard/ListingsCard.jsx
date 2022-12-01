fcff7167d1d31a626d575427a87e0e513be431c5
Michael Shimeles <michaelwasihun96@gmail.com>
2022-11-30 20:36:33 -0500

    Designed Listings card

---
+++ src/components/ListingsCard/ListingsCard.jsx
@@ -0,0 +1,31 @@
+import "./ListingsCard.scss";
+import MoonBird from "../../assets/images/moonbird.png";
+const ListingsCard = () => {
+  return (
+    <div className="listings">
+      <div className="listings__container">
+        <img className="listings__img" src={MoonBird} />
+        <div className="listings__info">
+          <div className="listings__stats-left">
+            <div className="listings__stats-id">
+              <p>#9682</p>
+            </div>
+            <div className="listings__stats-rank">
+              <p>Rank: 1242</p>
+            </div>
+          </div>
+          <div className="listings__stats-right">
+            <div className="listings__stats-price">
+              <p>Ξ73</p>
+            </div>
+            <div className="listings__stats-buy">
+              <p>Buy</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListingsCard;
--- src/pages/Terminal/Terminal.jsx
+++ src/pages/Terminal/Terminal.jsx
@@ -1,12 +1,14 @@
 import NavBar from "../../components/NavBar/NavBar"
 import CollectionBio from "../../components/CollectionBio/CollectionBio"
 import StatsBar from "../../components/StatsBar/StatsBar"
+import ListingsCard from "../../components/ListingsCard/ListingsCard"
 const Terminal = () => {
     return (
         <div className="terminal">
             <NavBar />
             <CollectionBio />
             <StatsBar />
+            <ListingsCard />
         </div>
     )
 }
