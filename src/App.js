91e63a3dbc9d8403a882e9834ce33d8e0f752319
Michael Shimeles <michaelwasihun96@gmail.com>
2022-12-07 16:06:54 -0500

    404 reroutes to homepage

--- src/App.js
+++ src/App.js
@@ -4,48 +4,37 @@ import Terminal from "./pages/Terminal/Terminal";
 import "./App.scss";
 import Portfolio from "./pages/Portfolio/Portfolio";
 import HotMints from "./pages/HotMints/HotMints";
-import {
-  getDefaultWallets,
-  RainbowKitProvider,
-  darkTheme,
-} from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
-import "@rainbow-me/rainbowkit/styles.css";
+import { WagmiConfig, createClient } from "wagmi";
+import { ConnectKitProvider, getDefaultClient } from "connectkit";
 
-const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-  [
-    alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_ID }),
-    publicProvider(),
-  ]
+const alchemyId = process.env.REACT_APP_ALCHEMY_ID;
+
+const client = createClient(
+  getDefaultClient({
+    appName: "Exodus",
+    alchemyId,
+  })
 );
-
-const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
-  chains,
-});
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
 
 function App() {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
+    <WagmiConfig client={client}>
+      <ConnectKitProvider
+        theme="midnight"
+        customTheme={{
+          "--ck-font-family": '"Open Sans", sans-serif',
+        }}
+      >
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/collection/:id" element={<Terminal />} />
             <Route path="/portfolio/:id" element={<Portfolio />} />
             <Route path="/hotmints" element={<HotMints />} />
+            <Route path="*" element={<Homepage />} />
           </Routes>
         </BrowserRouter>
-      </RainbowKitProvider>
+      </ConnectKitProvider>
     </WagmiConfig>
   );
 }
